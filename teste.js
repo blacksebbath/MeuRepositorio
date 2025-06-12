8ecc1ceaa0aeecc25be0be8cf87e4862e686bb46
blacksebbath <fredericosebba7@gmail.com>
2025-06-11 21:02:53 -0300

    Meu primeiro commit

---
+++ teste.js
@@ -0,0 +1,12 @@
+<script>
+
+let numero1 = parseFloat(prompt("Digite o primeiro número: "));
+
+let numero2 = parseFloat(prompt("Digite o segundo número: "));
+
+let soma = numero1 + numero2;
+
+alert("A soma de " + numero1 + " + " + numero2 + " é: " + soma);
+
+
+</script>
